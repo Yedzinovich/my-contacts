414e8974aad85c3eb7c36fbecac56d226d18d59a
Inna <innayedzinovich@gmail.com>
2020-07-15 15:30:18 -0400

    sckeletin for ContactForm created. 54/12:28

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -26,7 +26,7 @@ Navbar.propTypes = {
 };
 
 Navbar.defaultProps = {
-  title: 'Contact Keeper',
+  title: 'My Contacts',
   icon: 'fas fa-id-card-alt',
 };
 
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import ContactReducer from './contactReducer';
 
@@ -28,6 +28,10 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(ContactReducer, initialState);
 
   // Add Contact
+  const addContact = (contact) => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
 
